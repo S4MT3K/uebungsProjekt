44da46a5e7752b65e7b076ac3f41f83bb76a5228
s4mt3k <3rdlvl@gmx.de>
2025-03-21 11:32:26 +0100

    Added createdb.sql

---
+++ Createdb.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE User_2406;
+
+USE User_2406;
+
+CREATE TABLE User (
+                      Id INT AUTO_INCREMENT PRIMARY KEY,
+                      Vorname VARCHAR(50),
+                      Nachname VARCHAR(51),
+                      age TINYINT
+);
